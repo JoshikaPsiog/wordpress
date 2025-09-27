db100a52386bd7aec28ebe2d6256ca532bb4f807
joshika <joshika.mahendrababu@psiog.com>
2025-09-27 22:36:50 +0530

    3 buttons

--- java.js
+++ java.js
@@ -1,9 +1,20 @@
 class Editor {
   constructor(editorId) {
     this.editor = document.getElementById(editorId);
+    this.initToolbar();
+  }
+
+  initToolbar() {
+    document.querySelectorAll(".toolbar button").forEach(button => {
+      button.addEventListener("click", () => {
+        const command = button.dataset.command;
+        document.execCommand(command, false, null);
+      });
+    });
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   new Editor("editor");
 });
+
